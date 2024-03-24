815a57881459ce0ed3a19159fe5da0a464eac336
khatunsania <khatunsania403@gmail.com>
2024-03-24 14:08:22 +0530

    line hight added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 			<div className="w-[70%] m-auto py-40 lg:flex lg:justify-between md:block ">
 				<div className="lg:w-[40%] md:w-[100%] w-[100%]">
 					<h3 className="text-xl text-[#fff] py-4 ">By sania khatun</h3>
-					<h1 className="lg:text-5xl md:text-4xl text-2xl  w-[100%]   text-[#fff] font-bold ">
+					<h1 className="lg:text-5xl md:text-4xl text-2xl  leading-tight w-[100%]   text-[#fff] font-bold ">
 						The best website tamplate layout for your buisness
 					</h1>
 					<p className="lg:text-2xl md:text-2xl text-xl text-[#fff] py-5 md:w-[100%]">
@@ -20,11 +20,16 @@ const Hero = () => {
 							<li className="flex pr-4">
 								{" "}
 								<CalendarDays className="text-[#fff] lg:w-[20px] md:w-[20px] w-[15px] " />
-								<span className="pl-1 text-[#cd8a72] lg:text-base md:text-base sm:text-sm text-[12px] lg:mt-0 md:mt-0 mt-1"> 23 Mar,2024</span>
+								<span className="pl-1 text-[#cd8a72] lg:text-base md:text-base sm:text-sm text-[12px] lg:mt-0 md:mt-0 mt-1">
+									{" "}
+									23 Mar,2024
+								</span>
 							</li>
 							<li className="flex">
 								<Clock className="text-[#fff] lg:w-[20px] md:w-[20px] w-[15px] " />
-								<span className="pl-1 text-[#cd8a72] lg:text-base md:text-base sm:text-sm text-[12px] lg:mt-0 md:mt-0 mt-1">3 min read</span>
+								<span className="pl-1 text-[#cd8a72] lg:text-base md:text-base sm:text-sm text-[12px] lg:mt-0 md:mt-0 mt-1">
+									3 min read
+								</span>
 							</li>
 						</ul>
 					</div>
